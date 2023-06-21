938849f07df01cfd9ac389980110720a31f57303
AGM-12 BULLPUP <83917185+HalilKelew@users.noreply.github.com>
2023-06-21 11:30:51 -0500

    Add files via upload

---
+++ automobile.py
@@ -0,0 +1,7 @@
+class Automobile():
+    def __init__(self, make, model, vin, engine_size, owner):
+        self.make = make
+        self.model = model
+        self.vin = vin
+        self.engine_size = engine_size
+        self.owner = owner
---
+++ automobile_generator.py
@@ -0,0 +1,12 @@
+import automobile
+
+auto1 = automobile.Automobile("Mercury", "Sable", "1234", 3.0, "Bob")
+auto2 = automobile.Automobile("Honda", "Accord", "23456", 2.2, "Alice")
+
+auto_list = []
+auto_list.append(auto1)
+auto_list.append(auto2)
+
+for auto in auto_list:
+    print(auto.make)
+    print(auto.model)
